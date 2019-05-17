6763f4c359c528a76f90e42b52b10bc265711226
Scott H <zenwattage@gmail.com>
2019-05-17 14:44:43 -0700

    connected express server

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+//dependencies
 var express = require('express');
 var path = require('path');
 
+//setup express app
 var app = express();
+var PORT = 3000;
 
+//set up express app to handle data parsing
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.use(express.json());
+
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+})
+
+
+//start server listnening on port 3000
+app.listen(PORT, function() {
+    console.log("FriendFinder listening on PORT" + PORT);
+});
