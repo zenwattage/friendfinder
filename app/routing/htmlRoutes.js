c5468729d016a2f35f7151a5ed02de3ce812535a
Scott H <zenwattage@gmail.com>
2019-05-17 16:09:16 -0700

    intial buttons to home.html and survey.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,19 +10,18 @@ module.exports = function (app) {
 
   //* A default, catch-all route that leads to `home.html` which displays the home page.
   app.get('/home', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
+    res.sendFile(path.join(__dirname, '/../public/home.html'));
   });
 
 
   //* A GET Route to `/survey` which should display the survey page.
 
   app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/survey.html'));
+    res.sendFile(path.join(__dirname, '/../public/survey.html'));
   });
 
   app.use( function(req, res) {
-    res.sendFile(path.join(__dirname + '../public/home.html'))
+    res.sendFile(path.join(__dirname + '/../public/home.html'))
   })
 
 }
-
