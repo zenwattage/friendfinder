102be17dc1aaf3f712bfad736b336ed6cbfa2032
Scott H <zenwattage@gmail.com>
2019-05-17 15:47:43 -0700

    routes setup and linked to home html and survey html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,20 +2,22 @@
 //when user clicks link
 //the routing will determine which pages is delivered to the user 
 
+
+
 var path = require('path');
 
 module.exports = function (app) {
 
   //* A default, catch-all route that leads to `home.html` which displays the home page.
-  app.get("/home", function (req, res) {
-    res.sendFile(path.join(__dirname, './public/home.html'));
+  app.get('/home', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 
 
   //* A GET Route to `/survey` which should display the survey page.
 
-  app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
 
   app.use( function(req, res) {
--- server.js
+++ server.js
@@ -8,10 +8,6 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-})
-
 //set up express app to handle data parsing
 app.use(express.urlencoded({
     extended: true
@@ -19,11 +15,8 @@ app.use(express.urlencoded({
 
 app.use(express.json());
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted: Hello World\n')
-    res.end(JSON.stringify(req.body, null, 2))
-})
+//inclue route files
+require('./app/routing/htmlRoutes.js')(app);
 
 //start server listnening on port 8080
 app.listen(PORT, function () {
